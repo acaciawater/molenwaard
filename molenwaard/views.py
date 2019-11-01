ab2ea27cbb71f0b202b213fb1227b1c7794fe8d5
theo <tkleinen@gmail.com>
2019-11-01 09:34:33 +0100

    use view from bro app for download

--- molenwaard/views.py
+++ molenwaard/views.py
@@ -6,18 +6,11 @@ Created on Sep 1, 2016
 import json
 
 from django.conf import settings
-from django.http.response import JsonResponse, HttpResponseServerError,\
-    HttpResponse
+from django.http.response import JsonResponse, HttpResponseServerError
 from django.views.generic.detail import DetailView
 
 from acacia.meetnet.models import Network, Well
 from acacia.meetnet.views import NetworkView
-import zipfile
-from acacia.meetnet.bro.gmw import registration_request
-import StringIO
-from xml.etree.ElementTree import ElementTree
-from django.utils.text import slugify
-
 
 class HomeView(NetworkView):
     template_name = 'molenwaard/home.html'
@@ -51,20 +44,3 @@ def well_locations(request):
             return HttpResponseServerError(unicode(e))
     return JsonResponse(result,safe=False)
 
-
-def download_bro(request):
-    ''' download ZIP file with BRO registration requests for all wells '''
-    io = StringIO.StringIO()
-    zf = zipfile.ZipFile(io,'w')
-    try:
-        for well in Well.objects.all():
-            request = ElementTree(registration_request(well,kvk='73552739'))
-            xml = StringIO.StringIO()
-            request.write(xml,xml_declaration=True,encoding='utf-8')
-            zf.writestr(slugify(well.nitg or well.name) + '.xml', xml.getvalue())
-        zf.close()
-        resp = HttpResponse(io.getvalue(), content_type = "application/x-zip-compressed")
-        resp['Content-Disposition'] = 'attachment; filename=bro.zip'
-    except Exception as e:
-        resp = HttpResponseServerError(unicode(e))
-    return resp
